6f31daf9f5b4da002f95cdb0dad3fb272d23391e
HDefender <brziganshin@gmail.com>
2026-09-21 14:39:10 +0300

    see previous commit

--- src/main/java/util/JsonConverter.java
+++ src/main/java/util/JsonConverter.java
@@ -1,9 +1,6 @@
 package util;
 
 import com.google.gson.Gson;
-import dto.BaseDto;
-
-import java.util.List;
 
 public class JsonConverter {
     private static final Gson gson = new Gson();
@@ -11,10 +8,6 @@ public class JsonConverter {
     }
     public static String convertToJson(Object response) {
         return gson.toJson(response);
-    }
-
-    public static String convertToJson(List<? extends BaseDto> list) {
-        return gson.toJson(list);
     }
 
 }
