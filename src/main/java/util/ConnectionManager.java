107147201602c6229ca9b8c18857122ed03efbd4
HDefender <brziganshin@gmail.com>
2026-09-21 13:33:50 +0300

    fix connection pool initialization

--- src/main/java/util/ConnectionManager.java
+++ src/main/java/util/ConnectionManager.java
@@ -1,8 +1,8 @@
 package util;
 
 import com.zaxxer.hikari.HikariDataSource;
+import exception.DatabaseException;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -11,14 +11,21 @@ import java.sql.SQLException;
 public class ConnectionManager {
     public static final String URL_KEY = "db.url";
 
+    private static final HikariDataSource DATA_SOURCE;
+
     static {
         loadDriver();
+        try {
+            DATA_SOURCE = createDataSource();
+        } catch (SQLException e) {
+            throw new DatabaseException("Failure to create connection pool");
+        }
     }
 
     private ConnectionManager(){
     }
 
-    public static DataSource getDataSource() throws SQLException {
+    public static HikariDataSource createDataSource() throws SQLException {
         var dataSource = new HikariDataSource();
         dataSource.setJdbcUrl(PropertiesUtil.getProperty(URL_KEY));
         return dataSource;
@@ -26,17 +33,21 @@ public class ConnectionManager {
 
     public static Connection get (){
         try {
-            return getDataSource().getConnection();
+            return DATA_SOURCE.getConnection();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DatabaseException("Failure to get connection");
         }
+    }
+
+    public static void close() {
+        DATA_SOURCE.close();
     }
 
     private static void loadDriver() {
         try {
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Driver SQL is not found",e);
+            throw new DatabaseException("Driver SQL is not found");
         }
     }
 }
